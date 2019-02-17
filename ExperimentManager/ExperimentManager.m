9118e5715895f861a0c828b92ee6966f865bebbe
MaorG <maor.grinberg@mail.huji.ac.il>
2019-02-17 14:30:43 +0200

    minor changes + experiment config csv example

--- ExperimentManager/ExperimentManager.m
+++ ExperimentManager/ExperimentManager.m
@@ -4,6 +4,9 @@ classdef ExperimentManager
        pm 
        configFileName
        conf
+       rawData
+       perpData
+       
     end
     
     methods
@@ -22,12 +25,17 @@ classdef ExperimentManager
         end
         
         function obj = doWork(obj)
-            for i = 1:numel(conf.stage)
-                doStage(conf.stage{i})
+            for i = 1:numel(obj.conf.stage)
+                obj.doStage(obj.conf.stage{i})
             end
         end
         
         function obj = doStage(obj, stageName)
+            if (strcmp(stageName,'raw'))
+                disp('raw stage');
+            elseif (strcmp(stageName,'prep'))
+                disp('prep stage');
+            end
             
         end
         
