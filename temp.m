f9dd19321331ac3ae467abf3615696cdbe4c55fe
MaorG <maor.grinberg@mail.huji.ac.il>
2019-02-18 15:35:16 +0200

    a comment in temp.m

--- temp.m
+++ temp.m
@@ -6,9 +6,14 @@ em = ExperimentManager();
 
 em.configure();
 
-%em.doWork();
 em.doLoad();
+
+%todo michael: remove, just showing you how to view the state of things
+disp(em.dm.allData(1));
+
 em.doPrep();
+
+disp(em.dm.allData(1));
 
 end
 
