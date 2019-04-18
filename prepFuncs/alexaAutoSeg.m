5f665aeff9674493de0ee0bb3d6f784f5ba24d24
MaorG <maor.grinberg@mail.huji.ac.il>
2019-04-18 11:03:05 +0300

    alegaAutoSeg - playing with curvature

--- prepFuncs/alexaAutoSeg.m
+++ prepFuncs/alexaAutoSeg.m
@@ -5,10 +5,12 @@ props = parseParams(parameters);
 AL = data.(props.srcALX);
 BF = data.(props.srcBF);
 
-disk = getDisk(20);
+disk = getDisk(5);
 
-ALs = double(imgaussfilt(AL, 10));
-BFs = double(imgaussfilt(BF, 10));
+%ALs = double(imgaussfilt(AL, 10));
+%BFs = double(imgaussfilt(BF, 10));
+ALs = double(imgaussfilt(AL, 4));
+BFs = double(imgaussfilt(BF, 4));
 
 BFg = imgradient(BFs);
 ALg = imgradient(ALs);
@@ -21,9 +23,44 @@ ALg = imgradient(ALs);
 [BFgxx, BFgxy] = gradient(BFgx);
 [~, BFgyy] = gradient(BFgy);
 
-K = (ALgxx .* ALgyy - (ALgxy.^2)) ./  (1 + (ALgx.^2) + (ALgy.^2)).^2;
-H = ((1+ALgx.^2).*ALgyy + (1+ALgy.^2).*ALgxx - 2.*ALgx.*ALgy.*ALgxy)./...
-    ((1 + ALgx.^2 + ALgy.^2).^(3/2));
+Kal = (ALgxx .* ALgyy - (ALgxy.^2)) ./  (1 + (ALgx.^2) + (ALgy.^2)).^2;
+Kbf = (BFgxx .* BFgyy - (BFgxy.^2)) ./  (1 + (BFgx.^2) + (BFgy.^2)).^2;
+
+% H = ((1+ALgx.^2).*ALgyy + (1+ALgy.^2).*ALgxx - 2.*ALgx.*ALgy.*ALgxy)./...
+%     ((1 + ALgx.^2 + ALgy.^2).^(3/2));
+
+Jal = stdfilt(Kal,disk);
+Jbf = stdfilt(Kbf,disk);
+figure; imagesc(log10(Jal));
+figure; imagesc(log10(Jbf));
+
+
+mask = Jal < 0.0000001; % ????
+
+% figure; imshow(imfuse(mat2gray(BF,[20000,60000]), ...
+%     cat(3,bwperim(imfill((J < 0.00001),'holes')), ...
+%           bwperim(imfill((J < 0.0001),'holes')), ...
+%           bwperim(imfill((J < 0.001),'holes'))),'blend'))
+
+mask = imfill( mask ,'holes');
+figure; 
+imshow(imfuse(mat2gray(BF,[20000,60000]), ...
+    cat(3,bwperim(imfill( Jal < 0.00000001 ,'holes')), ...
+          bwperim(imfill( Jal < 0.0000001 ,'holes')), ...
+          bwperim(imfill( Jal < 0.000001 ,'holes'))),...
+          'blend'));
+      
+res = mask;
+
+return
+
+Kbf = (BFgxx .* BFgyy - (BFgxy.^2)) ./  (1 + (BFgx.^2) + (BFgy.^2)).^2;
+disk = getDisk(5);
+Jbf = stdfilt(Kbf,disk);
+
+
+figure;
+imagesc(log10(Jbf))
 
 
 ALgx = diff(ALs, 1, 1);
@@ -34,7 +71,7 @@ ALgxy = diff(ALgx, 1, 2);
 ALgyx = diff(ALgy, 1, 1);
 ALgyy = diff(ALgy, 1, 2);
 
-imshow(cat(3,0.5*mat2gray(ALs),0.5*mat2gray(BFs), 1*mat2gray(K,0.0001*[-1,1])))
+imshow(cat(3,0.5*mat2gray(ALg),0.5*mat2gray(BFg), 1*mat2gray(K,0.0001*[-1,1])))
 
 figure;
 imshow(mat2gray(BF));
