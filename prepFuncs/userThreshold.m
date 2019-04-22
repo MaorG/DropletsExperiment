8fe14083f4d73e4adbbedac67ed913e4d7c2854c
MaorG <maor.grinberg@mail.huji.ac.il>
2019-04-22 11:36:59 +0300

    manual input functions (simple threshold and guassian-curvature-variance)

--- prepFuncs/userThreshold.m
+++ prepFuncs/userThreshold.m
@@ -1,58 +1,23 @@
-function res = alexaAutoSeg(data,parameters)
+function res = userThreshold(data,parameters)
 
 props = parseParams(parameters);
 
 AL = data.(props.srcALX);
 BF = data.(props.srcBF);
 
-disk = getDisk(5);
-
-%ALs = double(imgaussfilt(AL, 10));
-%BFs = double(imgaussfilt(BF, 10));
-ALs = double(imgaussfilt(AL, 4));
-%BFs = double(imgaussfilt(BF, 4));
-
-%BFg = imgradient(BFs);
-ALg = imgradient(ALs);
-
-[ALgx, ALgy] = gradient(ALs);
-[ALgxx, ALgxy] = gradient(ALgx);
-[~, ALgyy] = gradient(ALgy);
-
-% [BFgx, BFgy] = gradient(BFs);
-% [BFgxx, BFgxy] = gradient(BFgx);
-% [~, BFgyy] = gradient(BFgy);
-
-Kal = (ALgxx .* ALgyy - (ALgxy.^2)) ./  (1 + (ALgx.^2) + (ALgy.^2)).^2;
-% Kbf = (BFgxx .* BFgyy - (BFgxy.^2)) ./  (1 + (BFgx.^2) + (BFgy.^2)).^2;
-
-% H = ((1+ALgx.^2).*ALgyy + (1+ALgy.^2).*ALgxx - 2.*ALgx.*ALgy.*ALgxy)./...
-%     ((1 + ALgx.^2 + ALgy.^2).^(3/2));
-
-Jal = stdfilt(Kal,disk);
-% Jbf = stdfilt(Kbf,disk);
-% figure; imagesc(log10(Jal));
-% figure; imagesc(log10(Jbf));
-% figure; imshow(imfuse(mat2gray(BF,[20000,60000]), ...
-%     cat(3,bwperim(imfill((J < 0.00001),'holes')), ...
-%           bwperim(imfill((J < 0.0001),'holes')), ...
-%           bwperim(imfill((J < 0.001),'holes'))),'blend'))
-
-
-th = 0.0000001
+th = 1000;
 
 h = figure;
-userinput = 0
-clim = [1,2^16 - 1];
+userinput = 0;
+clim = [0,2^16 - 1];
+jump = 1000;
 
 BFUI = BF;
-jump = 100;
+
 rectUI = [1,1,size(BF,1),size(BF,2)];
 
-interrupted = false;
-
 while userinput ~= 13
-    maskUI = Jal(rectUI(1):(rectUI(1)+rectUI(3)-1),rectUI(2):(rectUI(2)+rectUI(4)-1)) < th; % ????
+    maskUI = AL(rectUI(1):(rectUI(1)+rectUI(3)-1),rectUI(2):(rectUI(2)+rectUI(4)-1)) > th; % ????
     maskUI = imfill( maskUI ,'holes');
     BFUI = BF(rectUI(1):(rectUI(1)+rectUI(3)-1),rectUI(2):(rectUI(2)+rectUI(4)-1));
     imshow(imfuse( ...
@@ -70,24 +35,18 @@ while userinput ~= 13
     continueToBigLoop = false;
     while ~continueToBigLoop
         title( [ '{q/a}-->[', num2str(clim(1)), ', ', num2str(clim(2)), ']<--{w/s}  {up/down} jump: ', num2str(jump), ',  {left/right} threshold: ', num2str(th)] );
-
         continueToBigLoop = false;
-        try
-            [x,y,button]=ginput(1)
-        catch
-             button = 13;
-             interrupted = true;
-        end
+        [x,y,button]=ginput(1)
         if isempty(button)
             button = 13;
         end
         button
         switch button
             case 28
-                th = th * jump;
+                th = th - jump;
                 continueToBigLoop = true;
             case 29
-                th = th / jump
+                th = th + jump
                 continueToBigLoop = true;
             case 31
                 jump = ceil(jump/10);
@@ -95,13 +54,14 @@ while userinput ~= 13
             case 30
                 jump = jump*10;
                 %continueToBigLoop = true;
-
             case 13
                 continueToBigLoop = true;
             case 122
-                rectUI = getrect;
-                rectUI = rectUI([2,1,4,3])
-                continueToBigLoop = true;
+                rectUItemp = getrect;
+                if rectUItemp(3) ~= 0 && rectUItemp(4) ~= 0 
+                    rectUI = rectUItemp([2,1,4,3]) + [rectUI(1),rectUI(2),0,0];
+                    continueToBigLoop = true;
+                end
             case 120
                 rectUI = [1,1,size(BF,1),size(BF,2)];
                 continueToBigLoop = true;
@@ -128,65 +88,12 @@ while userinput ~= 13
     end
     
 end
+close(h);
 
-if (~interrupted)
-    close(h);
-end
-
-mask = Jal < th; 
+mask = AL > th; 
 mask = imfill( mask ,'holes');
 
 res = mask;
-
-return
-
-Kbf = (BFgxx .* BFgyy - (BFgxy.^2)) ./  (1 + (BFgx.^2) + (BFgy.^2)).^2;
-disk = getDisk(5);
-Jbf = stdfilt(Kbf,disk);
-
-
-figure;
-imagesc(log10(Jbf))
-
-
-ALgx = diff(ALs, 1, 1);
-ALgy = diff(ALs, 1, 2);
-
-ALgxx = diff(ALgx, 1, 1);
-ALgxy = diff(ALgx, 1, 2);
-ALgyx = diff(ALgy, 1, 1);
-ALgyy = diff(ALgy, 1, 2);
-
-imshow(cat(3,0.5*mat2gray(ALg),0.5*mat2gray(BFg), 1*mat2gray(K,0.0001*[-1,1])))
-
-figure;
-imshow(mat2gray(BF));
-
-[x,y] = ginput();
-
-figure;
-scatter(((K(:))), BFg(:))
-hold on
-
-idx = uint32(sub2ind(size(ALg), y,x));
-
-scatter(K(idx), BFg(idx),100,'r');
-
-mmm = (K > min(K(idx)) & K< max(K(idx)) & (BFg > min(BFg(idx)) & BFg < max(BFg(idx)) ));
-
-figure
-imshow(cat(3,0.5*mat2gray(ALg),0.5*mat2gray(BFg), 2*mat2gray(mmm)))
-
-figure
-scatter(K(:), BFg(:));
-hold on
-scatter(K(mmm(:)>0), BFg(mmm(:)>0));
-
-
-
-
-res = [];
-
 end
 
 function disk = getDisk(radius)
