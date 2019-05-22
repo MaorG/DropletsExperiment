13bf56e38636b215bbf92ba98f48d5817ffb15b0
MaorG <maor.grinberg@mail.huji.ac.il>
2019-05-22 22:01:14 +0300

    fixed userThreshold out of bound error

--- temp.m
+++ temp.m
@@ -4,11 +4,13 @@ init
 
 em = ExperimentManager();
 
-conf = '\\QNAP01\LongTerm\Maor\14.5.19 p130519mix1\exp_mix_201_manual.csv'
-conf = '\\QNAP01\LongTerm\Maor\14.5.19 p130519mix1\exp_mix_201_phase2.csv'
-conf = '\\QNAP01\LongTerm\Maor\14.5.19 p130519mix1\exp_mix_201_phase3.csv'
+% conf = '\\QNAP01\LongTerm\Maor\14.5.19 p130519mix1\exp_mix_201_manual.csv'
+% conf = '\\QNAP01\LongTerm\Maor\14.5.19 p130519mix1\exp_mix_201_phase2.csv'
+% conf = '\\QNAP01\LongTerm\Maor\14.5.19 p130519mix1\exp_mix_201_phase3.csv'
+% 
+% conf = '\\QNAP01\LongTerm\Maor\14.5.19 p130519mix1\exp_mix_201_manual_second_dry.csv'
 
-conf = '\\QNAP01\LongTerm\Maor\14.5.19 p130519mix1\exp_mix_201_manual_second_dry.csv'
+conf = '\\QNAP01\LongTerm\Maor\15.5.19 p130519memory2\exp_memory101_phase1.csv'
 
 em.configure(conf);
 
