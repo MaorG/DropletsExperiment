0b007cc461a9c5049a71c7c4e580ac172820ed43
MaorG <maor.grinberg@mail.huji.ac.il>
2019-05-28 15:44:54 +0300

    adding parametrization to displayErrorBars.m

--- outputFuncs/displayErrorBars.m
+++ outputFuncs/displayErrorBars.m
@@ -1,5 +1,31 @@
-function displayErrorBars(m, properties)
+function displayErrorBars(m, parameters)
 
-errorbar(m.X,m.Y,m.Yste)
-set(gca,'xscale','log');
+props = parseParams(parameters);
+
+if(isfield(m, 'Yste'))
+    errorbar(m.X,m.Y,m.Yste)
+else
+    errorbar(m.X,m.Y,m.Yerr)
+end
+
+set(gca,'xscale',props.xscale);
+set(gca,'yscale',props.yscale);
+end
+
+function props = parseParams(v)
+% default:
+props = struct(...
+    'xscale','linear',...
+    'yscale','linear' ...
+    );
+
+for i = 1:numel(v)
+    
+    if (strcmp(v{i}, 'xscale'))
+        props.xscale = v{i+1};
+    elseif (strcmp(v{i}, 'yscale'))
+        props.yscale = v{i+1};
+    end
+end
+
 end
