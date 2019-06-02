36e9c5194771c21ab5ac3a03986078888d66def0
Michael B <michael.labtory@gmail.com>
2019-06-03 00:36:51 +0300

    refactoring and linking with handles

--- DataManager/DataEntry.m
+++ DataManager/DataEntry.m
@@ -1,7 +1,14 @@
 classdef DataEntry < dynamicprops
+    
+    properties
+            parameters
+            uniqueID
+    end
+    
     methods
         function obj = DataEntry()
             
         end
     end
 end
+
